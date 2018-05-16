300b62975788870a976e94b91973e60558e5955e
이진영 <ijin-yeong@ijin-yeong-ui-MacBook-Pro.local>
2018-05-16 20:35:13 +0900

    init

---
+++ app/src/main/java/com/curonsys/army/GeneralUserActivity.java
@@ -0,0 +1,13 @@
+package com.curonsys.army;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeneralUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_user);
+    }
+}
---
+++ app/src/main/java/com/curonsys/army/MainActivity.java
@@ -0,0 +1,37 @@
+package com.curonsys.army;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1,btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ManagerUserActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),GeneralUserActivity.class);
+                startActivity(intent);
+                
+            }
+        });
+    }
+}
